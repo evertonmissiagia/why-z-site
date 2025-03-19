ac22d3652cfe7a2d8d6838f5fb50cf3d6965411e
Everton Misiagia <eamissiagia@gmail.com>
2025-03-18 21:31:35 -0300

    Adicionando reconhecimento e exibição da imagem enviada pelo usuário no cadastro

--- scripts/register.js
+++ scripts/register.js
@@ -21,4 +21,34 @@ document.getElementById('enviar-foto-input').addEventListener('change', (event) 
     };
 });
 
+function lerConteudoDoArquivo(arquivo) {
+    return new Promise ((resolve, reject) => {
+        const leitorArquivo = new FileReader();
+            leitorArquivo.onload = () => {
+                resolve({url: leitorArquivo.result, nome: arquivo.name});
+            };
 
+        leitorArquivo.onerror = () => {
+            reject('Erro ao ler o arquivo');
+        };
+
+        leitorArquivo.readAsDataURL(arquivo);
+    });
+}
+
+const placeholderImage = document.getElementById('placeholder-image');
+const placeholderImageName = document.getElementById('placeholder-image-name');
+
+inputFoto.addEventListener('change', async (evento) => {
+    const arquivoEnviado = evento.target.files[0];
+
+    if (placeholderImage) {
+        try {
+            const conteudoDaImagem = await lerConteudoDoArquivo(arquivoEnviado);
+            placeholderImage.src = conteudoDaImagem.url;
+            placeholderImageName.innerHTML = conteudoDaImagem.nome;
+        } catch (erro) {
+            console.error("Erro na leitura do arquivo")
+        }
+    }
+});
